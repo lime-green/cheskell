afdca29bd7832e23186e0b3aa5bdbc350a5d9445
Josh Doncaster-Marsiglio <jmarsiglio@gmail.com>
2015-02-01 22:05:36 -0500

    custom checkmate setup

--- js/board.js
+++ js/board.js
@@ -39,6 +39,7 @@ window.addEventListener('load', function () {
     // setup board
     game_alert.style.display = "none";
     chess.newGame();
+    chess.setFEN("8/8/8/8/1q6/2k5/8/K7 w - - 0 1");
 });
 
 var ViewModel = function (hash) {
